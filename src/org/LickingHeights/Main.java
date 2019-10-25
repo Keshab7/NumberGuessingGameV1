a353021edbd82747367dde62726ac97539f56114
Keshab <keshab.bista.2020@student.lhschools.org>
2019-10-25 10:01:42 -0400

    Random Save

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -7,17 +7,23 @@ public class Main {
     public static void main(String[] args) {
         int userNumber;
         int correctNumber;
-        correctNumber=7;
+        correctNumber = 7;
         Scanner Keyboard = new Scanner(System.in);
+        System.out.println();
         do {
             System.out.println("Guess a number 1-100");
             userNumber = Keyboard.nextInt();
-            if (userNumber > 7) {
-                System.out.println("Too High, Try Again");
-            } else if (userNumber < 7) {
-                System.out.println("Too Low, Try Again");
-            } else
-                System.out.println("Perfect");
-        }while (userNumber != correctNumber );
+
+        } while (userNumber != correctNumber);
     }
+
+    public static String randomNumberGuessing(int userNumber, int correctNumber){
+        if (userNumber > 7) {
+            System.out.println("Too High, Try Again");
+        } else if (userNumber < 7) {
+            System.out.println("Too Low, Try Again");
+        } else
+            System.out.println("Perfect");
+    }
+
 }
