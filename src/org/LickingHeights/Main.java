8ef3f87e415f4aa319cc569fd69de0da28c02a5e
Keshab <keshab.bista.2020@student.lhschools.org>
2019-10-25 09:22:47 -0400

    Do While Logic Loop

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -1,8 +1,23 @@
 package org.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int userNumber;
+        int correctNumber;
+        correctNumber=7;
+        Scanner Keyboard = new Scanner(System.in);
+        do {
+            System.out.println("Guess a number 1-100");
+            userNumber = Keyboard.nextInt();
+            if (userNumber > 7) {
+                System.out.println("Too High, Try Again");
+            } else if (userNumber < 7) {
+                System.out.println("Too Low, Try Again");
+            } else
+                System.out.println("Perfect");
+        }while (userNumber != correctNumber );
     }
 }
